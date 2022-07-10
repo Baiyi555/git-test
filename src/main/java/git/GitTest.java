0e9f6354ac27d9e8a619913e12a9f88a00d35e86
Fusheng <2531170133@qq.com>
2022-07-10 19:46:23 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author 余生
+ * @createDate 2022/7/10 19:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
